4ab2be0d62211a4749f44d3d69543a06265bdf6f
jdoubleu <hi@jdoubleu.de>
2022-03-20 15:23:33 +0100

    setup unit tests
    Following the example at https://github.com/espressif/esp-idf/tree/master/examples/system/unit_test

--- esp_idf/main/esp_idf.c
+++ esp_idf/main/esp_idf.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <unity.h>
+
+TEST_CASE("tzset", "[newlib]")
+{
+    TEST_ASSERT( true );
+}
 
 void app_main(void)
 {
+    UNITY_BEGIN();
+    unity_run_all_tests();
+    UNITY_END();
 
+    unity_run_menu();
 }
